2a9cc132380cbeb240a73350eed3669854e06f02
Denys Zaluzhnyi <denys.zaluzhnyi@gmail.com>
2024-06-06 19:32:36 +0200

    send message back

--- app/main.py
+++ app/main.py
@@ -6,7 +6,8 @@ def main():
     print("Logs from your program will appear here!")
 
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()
+    connection, address = server_socket.accept()
+    connection.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
 
 
 if __name__ == "__main__":
