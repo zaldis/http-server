426829348a66bfe8f8f417d44762218eda64e987
Denys Zaluzhnyi <denys.zaluzhnyi@gmail.com>
2024-06-08 10:04:24 +0200

    add file endpoint

--- app/main.py
+++ app/main.py
@@ -1,5 +1,8 @@
+import argparse
 import socket as sk
 import re
+from pathlib import Path
+from typing import Optional
 
 from threading import Thread
 
@@ -12,7 +15,10 @@ CLOSED_CONNECTION_MESSAGE = ""
 
 
 ECHO_ENDPOINT_PATTERN = re.compile(r"/echo/(.*)")
-USER_AGENT_PATTERN = re.compile(r"/user-agent")
+USER_AGENT_ENDPOINT_PATTERN = re.compile(r"/user-agent")
+FILE_ENDPOINT_PATTERN = re.compile(r"/files")
+
+BASE_DIR: Optional[Path] = None
 
 
 def run_http_server() -> None:
@@ -46,24 +52,58 @@ def handle_http_request(client_socket: sk.socket) -> None:
                 client_socket.send(_build_echo_message(message))
                 continue
 
-            if USER_AGENT_PATTERN.match(request_target):
+            if USER_AGENT_ENDPOINT_PATTERN.match(request_target):
                 agent_items = [row for row in request_data if 'user-agent: ' in row.lower()]
                 if len(agent_items) == 1:
                     message = agent_items[0][len('user-agent: ')-1:].strip()
                     client_socket.send(_build_echo_message(message))
                     continue
 
+            if FILE_ENDPOINT_PATTERN.match(request_target):
+                if file_name_match := re.compile(r".*/files/(.*)").match(request_target):
+                    file_name = file_name_match.group(1)
+                    try:
+                        with open(BASE_DIR / file_name, "rb") as file:
+                            data = file.read()
+                    except FileNotFoundError:
+                        client_socket.send(NOT_FOUND_MESSAGE)
+                    else:
+                        client_socket.send(_build_bytes_message(data))
+                    continue
+
             client_socket.send(NOT_FOUND_MESSAGE)
 
 
 def _build_echo_message(message: str) -> bytes:
-    body_size = str(len(message)).encode("utf-8")
+    message_size = str(len(message)).encode("utf-8")
     encoded_body = message.encode("utf-8")
-    return b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + body_size + b"\r\n\r\n" + encoded_body
+    return (
+        b"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: " + message_size + b"\r\n\r\n" + encoded_body
+    )
+
+
+def _build_bytes_message(message: bytes) -> bytes:
+    message_size = str(len(message)).encode("utf-8")
+    return (
+        b"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: "
+        + message_size + b"\r\n\r\n" + message
+    )
 
 
 def main():
     print("Logs from your program will appear here!")
+
+    parser = argparse.ArgumentParser(
+        prog="Zaldis HTTP Server",
+        description="Custom HTTP Server developed by Zaldis"
+    )
+    parser.add_argument(
+        '-d', '--directory'
+    )
+    args = parser.parse_args()
+
+    global BASE_DIR
+    BASE_DIR = Path(args.directory)
 
     try:
         run_http_server()
